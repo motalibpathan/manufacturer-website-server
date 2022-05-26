ab3eb285d619c86ff120535aec1aa6b48efb22c3
motalibpathan11 <73628059+motalibpathan11@users.noreply.github.com>
2022-05-26 15:36:52 +0600

    some changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
